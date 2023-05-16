083c5bad8b728a9ae07ce7d6cf39fb29b5440165
Ahmed Bahgat <ahbahgat01@gmail.com>
2023-05-17 01:25:19 +0300

    added generic interface and implementation IEntityRepo, EntityRepo that handles CRUD operations

---
+++ FoodOrderSystemAPI.DAL/Data/Repos/Interfaces/IEntityRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace FoodOrderSystemAPI.DAL.Data.Repos.Interfaces;
+
+public interface IEntityRepo<T> where T : class
+{
+    T? GetById(int id);
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    IEnumerable<T> Search(Expression<Func<T, bool>> predicate);
+}
