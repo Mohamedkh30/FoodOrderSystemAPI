0219662d759ce54f843cd66e5aa6b8d28e2b16f0
mohandayman <66260018+mohandayman@users.noreply.github.com>
2023-05-16 22:04:35 +0300

    Create Folders

---
+++ FoodOrderSystemAPI.DAL/Data/Models/CustomerModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderSystemAPI
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        [NotMapped]
+        public Location? Location { get; set; }
+
+
+        public string CreditCard { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<OrderModel> Orders { get; set; } = new HashSet<OrderModel>();
+        public ICollection<ReviewModel> Reviews { get; set; } = new HashSet<ReviewModel>();
+
+
+
+
+
+
+    }
+}
