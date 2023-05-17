ab80561e011606b91bb42a5c104cf55d6828b907
Mohamedkh30 <mohamed_khaled_1999@yahoo.com>
2023-05-17 15:15:18 +0300

    added Product Model & fullProduct DTO

---
+++ FoodOrderSystemAPI.BL/DTOs/Product/FullProductDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderSystemAPI.BL.DTOs.Product
+{
+    internal class FullProductDto
+    {
+        public int ID;
+
+        public String Productname = "";
+
+        public float price;
+
+        public String describtion = "";
+
+        public String img = "";
+
+        public float offer;
+
+        public float rate;
+
+        public String type = "";
+
+        public RestaurantModel restaurant;
+    }
+}
--- FoodOrderSystemAPI.DAL/Data/Models/ProductModel.cs
+++ FoodOrderSystemAPI.DAL/Data/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +9,28 @@ namespace FoodOrderSystemAPI;
 
 public class ProductModel
 {
+    public int ID;
+
+    public String Productname = "";
+
+    [Range(0f, float.MaxValue)]     //only +ve values
+    public float price;         
+
+    public String describtion = "";
+
+    public String img = "";
+
+    [Range(0f,1f)]
+    public float offer;    
+    
+    [Range(0f,5f)]
+    public float rate;    
+    
+    public String type = "";
+
+    public RestaurantModel restaurant;
+
+    public ICollection<OrderProductModel> orderProducts = new HashSet<OrderProductModel>();
+
+    public ICollection<ReviewModel> reviews = new HashSet<ReviewModel>();
 }
