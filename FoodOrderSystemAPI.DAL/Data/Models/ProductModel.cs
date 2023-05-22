880a4c50d6e9732830743daf05adeba17f0e6d79
Mohamedkh30 <mohamed_khaled_1999@yahoo.com>
2023-05-22 12:48:00 +0300

    edited product model

--- FoodOrderSystemAPI.DAL/Data/Models/ProductModel.cs
+++ FoodOrderSystemAPI.DAL/Data/Models/ProductModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace FoodOrderSystemAPI;
 public class ProductModel
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ProductId { set; get; }
 
     public String Productname { set; get; }  = "";
