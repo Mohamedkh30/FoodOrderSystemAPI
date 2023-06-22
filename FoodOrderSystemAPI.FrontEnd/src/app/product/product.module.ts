3ce2598fbd1b7846aaae1793a2de74ac8a1cc34c
Ahmed Bahgat <ahbahgat01@gmail.com>
2023-06-22 17:00:07 +0300

    created product dto, service and created config service

--- FoodOrderSystemAPI.FrontEnd/src/app/product/product.module.ts
+++ FoodOrderSystemAPI.FrontEnd/src/app/product/product.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
-
 @NgModule({
-  declarations: [
-    ProductDetailsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductDetailsComponent],
+  imports: [CommonModule],
 })
-export class ProductModule { }
+export class ProductModule {}
