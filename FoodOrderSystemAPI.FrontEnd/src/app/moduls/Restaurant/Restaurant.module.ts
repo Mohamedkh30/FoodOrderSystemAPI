4ab7b2732059fddec490dcad4c1f3593421e0315
Mahmoud <mahmoudgawad@hotmail.com>
2023-06-23 17:44:24 +0300

    Updated again and again

--- FoodOrderSystemAPI.FrontEnd/src/app/moduls/Restaurant/Restaurant.module.ts
+++ FoodOrderSystemAPI.FrontEnd/src/app/moduls/Restaurant/Restaurant.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RestaurantRoutingModule } from './restaurant-routing.module';
-import { CustomersComponent } from 'src/app/home/customers/customers.component';
 
 @NgModule({
-    declarations: [ CustomersComponent ],
+    declarations: [],
     imports: [
         CommonModule,
         RestaurantRoutingModule
