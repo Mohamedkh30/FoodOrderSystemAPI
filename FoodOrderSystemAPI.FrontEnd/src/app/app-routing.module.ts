70877bf4ca2a23552f0d995d476119b2f7e1773b
Ahmed Bahgat <ahbahgat01@gmail.com>
2023-06-23 16:18:58 +0300

    fixed routing for product and customer

--- FoodOrderSystemAPI.FrontEnd/src/app/app-routing.module.ts
+++ FoodOrderSystemAPI.FrontEnd/src/app/app-routing.module.ts
@@ -31,13 +31,13 @@ const Router: Routes = [
   //       (m) => m.HomeModule
   //     ),
   // },
-  {path:'home',component:HomePageComponent},
-  {path:'restaurant',component:RestaurantPageComponent},
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'**',component:NotFoundComponent},
-]
-
-
+  { path: 'home', component: HomePageComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'customer/:id', component: CustomerProfileDetailsComponent },
+  { path: 'restaurant', component: RestaurantPageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [],
